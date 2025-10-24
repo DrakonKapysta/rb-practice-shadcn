3b97d6bee0fef30366722f74b21e7e0cc44abef0
Oleh <o.matokin@rubylabs.com>
2025-10-24 18:33:57 +0300

    fix: fixed auth path

--- src/pkg/integrations/better-auth/auth-client.ts
+++ src/pkg/integrations/better-auth/auth-client.ts
@@ -1,2 +1,5 @@
+import { envClient } from '@/config/env'
 import { createAuthClient } from 'better-auth/react'
-export const authClient = createAuthClient()
+export const authClient = createAuthClient({
+  baseURL: envClient.NEXT_PUBLIC_CLIENT_WEB_URL,
+})
--- src/pkg/integrations/better-auth/auth.ts
+++ src/pkg/integrations/better-auth/auth.ts
@@ -3,6 +3,7 @@ import { drizzleAdapter } from 'better-auth/adapters/drizzle'
 import { nextCookies } from 'better-auth/next-js'
 
 import { db } from '@/pkg/libraries/drizzle'
+import { envClient } from '@/config/env'
 
 export const auth = betterAuth({
   database: drizzleAdapter(db, { provider: 'pg' }),
@@ -10,4 +11,6 @@ export const auth = betterAuth({
     enabled: true,
   },
   plugins: [nextCookies()],
+  baseURL: envClient.NEXT_PUBLIC_CLIENT_WEB_URL,
+  allowedOrigins: [envClient.NEXT_PUBLIC_CLIENT_WEB_URL],
 })
